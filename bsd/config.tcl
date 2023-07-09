b256e86fdfee7f09e88048b922233fede43a6266
Christopher Hoover <ch@murgatroid.com>
2023-07-08 17:58:08 -0700

    checkpoint

--- bsd/config.tcl
+++ bsd/config.tcl
@@ -1,8 +1,7 @@
 set label "MURGATROID"
 set hostname "pdp1153"
 
-set ncyl 3000
-set disk_size [expr $ncyl * 512]
+set ncyl 3099
 
 set root_disk_type "ra"
 set root_disk_no 0
@@ -18,12 +17,13 @@ set swap_dev "$root_disk$swap_part"
 set swap_start $root_size
 set swap_size 32768
 
+set usr_part "d"
+set usr_dev "$root_disk$usr_part"
+set usr_start [expr $root_size + $swap_size]
+set usr_size 2703383
+
 set whole_part "c"
 set whole_dev "$root_disk$whole_part"
 set whole_start 0
-set whole_size $disk_size
+# whole_size determined automagically
 
-set usr_part "g"
-set usr_dev "$root_disk$usr_part"
-set usr_start [expr $root_size + $swap_size]
-set usr_size [expr $disk_size - $usr_start]
