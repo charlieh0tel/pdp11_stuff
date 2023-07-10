5a7abd5babced5974e69a22a037a0209206cc172
Christopher Hoover <ch@murgatroid.com>
2023-07-09 22:21:19 -0700

    .x

--- bsd/go.sh
+++ bsd/go.sh
@@ -5,13 +5,14 @@ set -o xtrace
 
 DISK=2.11BSD_patch457-root.ra92
 
-if [ -e "${DISK}.post-03-snap" ]; then
-    cp -p "${DISK}.post-03-snap" "${DISK}"
+if [ -e "${DISK}.post-04-snap" ]; then
+    cp -p "${DISK}.post-04-snap" "${DISK}"
 else
     ./00_build_disk.expect
     ./01_build_root.expect
     ./02_bootstrap_disk.expect
     ./03_build_usr.expect
-    cp -p "${DISK}" "${DISK}.post-03-snap"
+    ./04_build_kernel.expect
+    cp -p "${DISK}" "${DISK}.post-04-snap"
 fi
-./04_customize.expect
+./05_customize.expect
