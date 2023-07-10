b1e603ce393689c0aec627b8c8072b20ae18bf6b
Christopher Hoover <ch@murgatroid.com>
2023-07-10 12:27:41 -0700

    mostly working

--- bsd/go.sh
+++ bsd/go.sh
@@ -12,7 +12,7 @@ else
     ./01_build_root.expect
     ./02_bootstrap_disk.expect
     ./03_build_usr.expect
-    ./04_build_kernel.expect
+    ./04_kernel.expect
     cp -p "${DISK}" "${DISK}.post-04-snap"
 fi
 ./05_customize.expect
