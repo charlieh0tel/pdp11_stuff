543fa0033a60ef7677fa7521a8d5dd7f4a4dc983
Christopher Hoover <ch@murgatroid.com>
2023-07-10 23:04:23 -0700

    Reduces root part size and does more calcs for the user.

--- bsd/config.tcl
+++ bsd/config.tcl
@@ -14,20 +14,22 @@ set root_disk "$root_disk_type$root_disk_no"
 set root_part "a"
 set root_dev "$root_disk$root_part"
 set root_start 0
-set root_size [expr 215 * $sec_per_cyl]
+set root_ncyl 12
+set root_size [expr $root_ncyl * $sec_per_cyl]
 
 set swap_part "b"
 set swap_dev "$root_disk$swap_part"
 set swap_start $root_size
-set swap_size [expr 35 * $sec_per_cyl]
+set swap_ncyl 25
+set swap_size [expr $swap_ncyl * $sec_per_cyl]
 
 set usr_part "d"
 set usr_dev "$root_disk$usr_part"
 set usr_start [expr $root_size + $swap_size]
-set usr_size [expr 2849 * $sec_per_cyl]
+set usr_ncyl [expr $ncyl - $root_ncyl - $swap_ncyl]
+set usr_size [expr $usr_ncyl * $sec_per_cyl]
 
 set whole_part "c"
 set whole_dev "$root_disk$whole_part"
 set whole_start 0
-# whole_size determined automagically
-
+set whole_size [expr $ncyl * $sec_per_cyl]
