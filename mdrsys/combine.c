097398ea5b000dd8e6d45ec47d99ab3d8a7f5a51
Christopher Hoover <ch@murgatroid.com>
2023-10-01 19:58:17 -0700

    Cleanup

--- mdrsys/combine.c
+++ mdrsys/combine.c
@@ -10,42 +10,42 @@ int main(int argc, char **argv) {
     exit(1);
   }
 
-  int fd1 = open(argv[1], O_RDONLY);
-  if (fd1 < 0) {
+  int fd_l = open(argv[1], O_RDONLY);
+  if (fd_l < 0) {
     fprintf(stderr, "%s: open %s failed: %m\n", argv[0], argv[1]);
     exit(1);
   }
 
-  int fd2 = open(argv[2], O_RDONLY);
-  if (fd2 < 0) {
+  int fd_h = open(argv[2], O_RDONLY);
+  if (fd_h < 0) {
     fprintf(stderr, "%s: open %s failed: %m\n", argv[0], argv[2]);
     exit(1);
   }
 
   while (1) {
-    char cc1, cc2;
-    int nr1 = read(fd1, &cc1, 1);
-    if (nr1 < 0) {
+    char cc_l, cc_h;
+    int nr_l = read(fd_l, &cc_l, 1);
+    if (nr_l < 0) {
       fprintf(stderr, "%s: read file_l: %m\n", argv[0]);
       exit(1);
     }
     
-    int nr2 = read(fd2, &cc2, 1);
-    if (nr2 < 0) {
+    int nr_h = read(fd_h, &cc_h, 1);
+    if (nr_h < 0) {
       fprintf(stderr, "%s: read file_h: %m\n", argv[0]);
       exit(1);
     }
 
-    if (nr1 == 1 && nr2 == 1) {
-      write(1, &cc1, 1);
-      write(1, &cc2, 1);
-    } else if (nr1 == 0 && nr2 == 0) {
+    if (nr_l == 1 && nr_h == 1) {
+      write(1, &cc_l, 1);
+      write(1, &cc_h, 1);
+    } else if (nr_l == 0 && nr_h == 0) {
       exit(0);
-    } else if (nr1 == 0) {
+    } else if (nr_l == 0) {
       fprintf(stderr, "%s: file_l is shorter than file_h\n", argv[0]);
       exit(1);
     } else {
-      assert(nr2 == 0);
+      assert(nr_h == 0);
       fprintf(stderr, "%s: file_h is shorter than file_l\n", argv[0]);
       exit(1);
     }
